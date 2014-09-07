930d59abc90231a62553eea3a0d058b5f3217890
ecosme <edgar.cosme@gmail.com>
2014-09-07 15:26:42 -0500

    Create Plot02.R

---
+++ Plot02.R
@@ -0,0 +1,26 @@
+#Set English language
+Sys.setlocale("LC_TIME", "English");
+#Load data from csv file 
+iniData<-read.csv("household_power_consumption.txt", sep=";", na.strings="?", header=T);
+#Format Date column in order to easy sub setting information
+iniData$Date<-as.Date(iniData$Date, format="%d/%m/%Y");
+#Select range of dates
+tidyData2<-subset(iniData, subset=(iniData$Date=="2007-02-01" | iniData$Date=="2007-02-02"));
+#Concatenate data and time in first column 
+datetimevec<-paste(as.Date(tidyData2$Date), tidyData2$Time);
+#Formatting date time as POSIX 
+tidyData2$Date<-as.POSIXct(datetimevec);
+#Removing unused data
+tidyData2$Time<-NULL;
+tidyData2$Global_reactive_power<-NULL;
+tidyData2$Voltage<-NULL;
+tidyData2$Global_intensity<-NULL;
+tidyData2$Sub_metering_1<-NULL;
+tidyData2$Sub_metering_2<-NULL;
+tidyData2$Sub_metering_3<-NULL;
+#Creating graphic
+plot(tidyData2$Global_active_power~tidyData2$Date, type="l", ylab="Global Active Power (kilowatts)", xlab="");
+#Get the graphic from screen to a "device"
+dev.copy(png, file="plot2.png", height=480, width=480);
+#Closing device
+dev.off();
